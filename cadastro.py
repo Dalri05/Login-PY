dd5527db1e24405f46ccb6f3f6422af149da5072
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-03-14 12:40:34 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,15 +1,1 @@
-from app import *
 
-def Cadastro():
-    user = input("Digite seu usuario")
-    senha = input(("digite uma senha"))
-    confirm_senha=(input("Confirme sua senha"))
-
-    if confirm_senha == senha:
-        main()
-    else:
-        print("as senhas nao coiciden")
-        sleep(3)
-        system("clear")
-        Cadastro()
-Cadastro()
